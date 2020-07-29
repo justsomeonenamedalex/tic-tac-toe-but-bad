4ce9486151ed728fd423c18895d7ca3d00394e84
Alex Davis <8m3xlt@gmail.com>
2020-07-29 14:04:45 +0100

    added cli, restructured a few things

--- main.py
+++ main.py
@@ -22,13 +22,16 @@ lines = ([
 
 def humanize_grid(grid: list) -> list:
     replace = {0: "#", 1: "x", 2: "o"}
-    copy = grid.copy()
+    copy = deepcopy(grid)
     for row_num, row in enumerate(copy):
         for column_num, item in enumerate(row):
             copy[row_num][column_num] = replace[item]
-    return copy
 
-def check_win(grid: list) -> [bool, str]:
+    out = [" ".join(line) for line in copy]
+    return "\n".join(out)
+
+
+def check_win(grid: list) -> [bool, int]:
     for line in lines:
         a = grid[line[0][0]][line[0][1]]
         b = grid[line[1][0]][line[1][1]]
@@ -37,9 +40,9 @@ def check_win(grid: list) -> [bool, str]:
             if a == 0:
                 pass
             elif a == 1:
-                return True, "x"
+                return True, 1
             elif a == 2:
-                return True, "o"
+                return True, 2
     return False, ""
 
 
@@ -54,23 +57,71 @@ def best_move(grid: list, player: int):
         new_grid = deepcopy(grid)
         new_grid[empty_coords[0]][empty_coords[1]] = player
         is_win = check_win(new_grid)
-        if is_win[0]:
-            print(empty_coords)
+        if is_win[0] and is_win[1] == player:
+            # print(empty_coords)
             return empty_coords
 
     for empty_coords in empty:
-        print("No immediate win")
+        # print("No immediate win")
         new_grid = deepcopy(grid)
         new_grid[empty_coords[0]][empty_coords[1]] = player
         return best_move(new_grid, player)
 
 
-grid_a = ([
-    [0, 0, 0],
-    [0, 0, 0],
-    [0, 0, 0]
-])
+# grid_a = ([
+#     [2, 1, 1],
+#     [0, 2, 2],
+#     [0, 1, 1]
+# ])
+#
+# best_move(grid_a, 2)
+# for l in humanize_grid(grid_a):
+#     print(l)
 
-best_move(grid_a, 1)
-for l in humanize_grid(grid_a):
-    print(l)
+def cli():
+    grid = deepcopy(base_grid)
+    play_to_coords = {
+        "1": [0, 0],
+        "2": [0, 1],
+        "3": [0, 2],
+        "4": [1, 0],
+        "5": [1, 1],
+        "6": [1, 2],
+        "7": [2, 0],
+        "8": [2, 1],
+        "9": [2, 2]
+    }
+    print(humanize_grid(grid))
+
+    while True:
+
+        while True:
+            print("\nYour turn!")
+            play = input("Where do you want to play to?(1-9) ")
+            try:
+                coords = play_to_coords[play]
+                break
+            except KeyError:
+                print("That isnt a valid square")
+
+        grid[coords[0]][coords[1]] = 1
+
+        print(humanize_grid(grid))
+        is_win = check_win(grid)
+        if is_win[0]:
+            print(f"{is_win[1]} wins!")
+            break
+
+        print("\nAI's turn!")
+
+        ai_coords = best_move(grid, 2)
+        grid[ai_coords[0]][ai_coords[1]] = 2
+
+        print(humanize_grid(grid))
+        is_win = check_win(grid)
+        if is_win[0]:
+            print(f"{is_win[1]} wins!")
+            break
+
+
+cli()
